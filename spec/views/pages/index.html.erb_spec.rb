5e529c4ba25e0cd04b743fb19d03a7119b292d08
Ivar Vong <ivar@ivarvong.com>
2016-04-12 12:28:40 -0400

    remove view spec for pages#index

--- spec/views/pages/index.html.erb_spec.rb
+++ spec/views/pages/index.html.erb_spec.rb
@@ -1,28 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe "pages/index", type: :view do
-  before(:each) do
-    assign(:pages, [
-      Page.create!(
-        :name => "MyText",
-        :url => "MyText",
-        :css_selector => "MyText",
-        :user_id => 1
-      ),
-      Page.create!(
-        :name => "MyText",
-        :url => "MyText",
-        :css_selector => "MyText",
-        :user_id => 1
-      )
-    ])
-  end
 
-  it "renders a list of pages" do
-    render
-    assert_select "tr>td", :text => "MyText".to_s, :count => 2
-    assert_select "tr>td", :text => "MyText".to_s, :count => 2
-    assert_select "tr>td", :text => "MyText".to_s, :count => 2
-    assert_select "tr>td", :text => 1.to_s, :count => 2
-  end
 end
