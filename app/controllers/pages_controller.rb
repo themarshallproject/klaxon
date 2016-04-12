f1e9f061094fe04ec93434cdf8aa24dc2b4ce72b
Ivar Vong <ivar@ivarvong.com>
2016-04-12 12:26:57 -0400

    scaffold for Pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,58 @@
+class PagesController < ApplicationController
+  before_action :set_page, only: [:show, :edit, :update, :destroy]
+
+  # GET /pages
+  def index
+    @pages = Page.all
+  end
+
+  # GET /pages/1
+  def show
+  end
+
+  # GET /pages/new
+  def new
+    @page = Page.new
+  end
+
+  # GET /pages/1/edit
+  def edit
+  end
+
+  # POST /pages
+  def create
+    @page = Page.new(page_params)
+
+    if @page.save
+      redirect_to @page, notice: 'Page was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # PATCH/PUT /pages/1
+  def update
+    if @page.update(page_params)
+      redirect_to @page, notice: 'Page was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /pages/1
+  def destroy
+    @page.destroy
+    redirect_to pages_url, notice: 'Page was successfully destroyed.'
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_page
+      @page = Page.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def page_params
+      params.require(:page).permit(:name, :url, :css_selector, :user_id)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
 
+  resources :pages
   root 'static#index'
 
   get '/help' => 'static#help', as: :help
