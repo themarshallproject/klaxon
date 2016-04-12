fad0cd978d4a8aea5ffc11765a9ebadd01bec2e2
Ivar Vong <ivar@ivarvong.com>
2016-04-12 12:27:25 -0400

    db migration for pages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160408004451) do
+ActiveRecord::Schema.define(version: 20160412162646) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,17 @@ ActiveRecord::Schema.define(version: 20160408004451) do
   end
 
   add_index "app_settings", ["key"], name: "index_app_settings_on_key", using: :btree
+
+  create_table "pages", force: :cascade do |t|
+    t.text     "name"
+    t.text     "url"
+    t.text     "css_selector"
+    t.integer  "user_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "pages", ["user_id"], name: "index_pages_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
