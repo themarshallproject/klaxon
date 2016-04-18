68a7ddb0603eaf76c374101ce8dfe3ba343997b4
Andy Rossback <a.rossback@gmail.com>
2016-04-18 17:56:08 -0500

    tweaking the snap shot page

--- pull_dev_database.sh
+++ pull_dev_database.sh
@@ -10,4 +10,4 @@ curl -o ./backups/dev-latest.dump `heroku pg:backups public-url --app klaxon-dev
 
 echo " ----- "
 echo "Restoring backup to endrun_development"
-/Applications/Postgres.app/Contents/Versions/9.5/bin/pg_restore --verbose --clean --no-acl --no-owner -h localhost -U `whoami` -d klaxon_development ./backups/dev-latest.dump
+/Applications/Postgres.app/Contents/Versions/9.3/bin/pg_restore --verbose --clean --no-acl --no-owner -h localhost -U `whoami` -d klaxon_development ./backups/dev-latest.dump
