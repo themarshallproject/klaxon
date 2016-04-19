3aac346f0cf6b1d6fc6d21ff2d32c1d2179a3ac1
Andy Rossback <a.rossback@gmail.com>
2016-04-19 10:48:48 -0500

    move script to bottom of compare page

--- app/assets/javascripts/static.js
+++ app/assets/javascripts/static.js
@@ -1,29 +1,2 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-
-// <input class="klax-js-snapshot" type="radio" 
-// name="before" data-index="<%= index %>" 
-// data-type="before" value="<%= snapshot.id %>">
-
-
-$(document).ready(function() {
-	$(".klax-js-snapshot-before[data-index='0']").attr("disabled","");
-	$(".klax-js-snapshot-before[data-index='1']").click();
-	$(".klax-js-snapshot-after[data-index='0']").show().click();
-	$(".klax-js-snapshot-before").click(function(){
-		var index = $(this).data("index");
-		$(".klax-js-snapshot-after").each(function(){
-			var thisIndex = $(this).data("index");
-			if (index <= thisIndex) {
-				$(this).attr("disabled","");
-				$(this).css("display","none");
-			} else {
-				$(this).css("display","block");
-				$(this).removeAttr("disabled","");
-				if (thisIndex == (index - 1)) {
-					$(this).click();
-				}
-			}
-		});
-	});
-});
