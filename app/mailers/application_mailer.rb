a498f15261965de2a7aae01c82471719dad3b633
Jeremy B. Merrill <jeremy.merrill@nytimes.com>
2016-04-21 16:47:41 -0400

    send from an address spec'ed in env vars

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
   # TODO consider http://stackoverflow.com/a/8106387
-  default from: 'Klaxon <no-reply@newsklaxon.org>'
+  default from: "Klaxon <#{ENV["EMAIL_FROM_ADDRESS"]}>"
   layout 'mailer'
 end
