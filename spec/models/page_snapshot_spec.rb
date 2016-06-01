ccc8d3af183c1de5e9cae62dca417d1c34df763e
Ivar Vong <ivar@ivarvong.com>
2016-06-01 15:46:59 -0400

    spec for page_snapshot instance 'previous' method

--- spec/models/page_snapshot_spec.rb
+++ spec/models/page_snapshot_spec.rb
@@ -12,4 +12,15 @@ RSpec.describe PageSnapshot, type: :model do
     expect(snapshot.valid?).to be true
   end
 
+  it "can query the (directly) previous snapshot" do
+    snapshot1 = create(:page_snapshot, created_at: 3.minutes.ago)
+    snapshot2 = create(:page_snapshot, created_at: 2.minutes.ago)
+    snapshot3 = create(:page_snapshot, created_at: 1.minutes.ago)
+    snapshot4 = create(:page_snapshot, created_at: 0.minutes.ago)
+
+    expect(snapshot2.previous).to eq snapshot1
+    expect(snapshot3.previous).to eq snapshot2
+    expect(snapshot4.previous).to eq snapshot3
+  end
+
 end
