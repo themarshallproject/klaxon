e9a32749d9332a2d8eb6382524ed11dcf3ffd35e
Andy Rossback <arossback@themarshallproject.org>
2016-06-24 16:09:03 -0400

    revert dev database script

--- pull_dev_database.sh
+++ pull_dev_database.sh
@@ -2,12 +2,12 @@
 set -e
 
 echo "Creating backup ..."
-heroku pg:backups capture --app rssbck-klaxon
+heroku pg:backups capture --app klaxon-dev
 
 echo " ----- "
 echo "Downloading backup ..."
-curl -o ./backups/dev-latest.dump `heroku pg:backups public-url --app rssbck-klaxon`
+curl -o ./backups/dev-latest.dump `heroku pg:backups public-url --app klaxon-dev`
 
 echo " ----- "
 echo "Restoring backup to endrun_development"
-/Applications/Postgres.app/Contents/Versions/9.5/bin/pg_restore --verbose --clean --no-acl --no-owner -h localhost -U `whoami` -d klaxon_development ./backups/dev-latest.dump
+/Applications/Postgres.app/Contents/Versions/9.3/bin/pg_restore --verbose --clean --no-acl --no-owner -h localhost -U `whoami` -d klaxon_development ./backups/dev-latest.dump
