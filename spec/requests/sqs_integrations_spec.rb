d8a014b5949a1f8189cf75dd0a4f0f513fd6c64d
Jeremy B. Merrill <jeremy.merrill@nytimes.com>
2016-11-21 13:58:16 -0500

    SQS integration now has tests

---
+++ spec/requests/sqs_integrations_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe "SqsIntegrations", type: :request do
+  describe "GET /sqs_integrations" do
+    it "works! (now write some real specs)" do
+      get sqs_integrations_path
+      expect(response).to have_http_status(200)
+    end
+  end
+end
