3148a30173824405f11c5811783970ababbb9056
Justin Myers <jmyers@ap.org>
2017-05-30 16:56:45 -0500

    Assign `@current_user` instance variable in tests
    For consistency with 8d19a539f52778a30f62cc4279207b64b5b33759.

--- spec/views/users/edit.html.erb_spec.rb
+++ spec/views/users/edit.html.erb_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe "users/edit", type: :view do
   before(:each) do
     @user = create(:user)
+    @current_user = @user
   end
 
   it "renders the edit user form" do
