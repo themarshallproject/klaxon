00d212c7bd4f3d3ecdcb67f4c1df0281c4f17763
Bob <rweston@ap.org>
2017-06-12 17:07:29 -0400

    removes unknown_user and expired_token method frm static controller and removes before_filter that allowed their use without authorize.

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,17 +1,9 @@
 class StaticController < ApplicationController
-  before_filter :authorize, except: [:unknown_user, :expired_token]
 
   def help
     path = File.join(Rails.root, 'data', 'help.md')
     markdown = File.read(path)
     @html = Kramdown::Document.new(markdown).to_html
-  end
-
-  def unknown_user
-  end
-
-  def expired_token
-    @user = User.find(params[:user_id].to_i)
   end
 
   def feed
