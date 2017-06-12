c2a5dd95daf0a7f4db56a92d98acfbf430a2790e
Bob <rweston@ap.org>
2017-06-12 17:06:07 -0400

    adds unknown_user and expired_token methods to sessions controller (from static)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -28,6 +28,13 @@ class SessionsController < ApplicationController
       redirect_to unknown_user_path
     end
   end
+  
+  def unknown_user
+  end
+
+  def expired_token
+    @user = User.find(params[:user_id].to_i)
+  end
 
   def destroy
     cookies.delete(:user_id)
