b53d0690334bdfb828c2425a2aba2b66d08c035a
Matthew Verive <immewnity@users.noreply.github.com>
2021-12-30 12:13:19 -0600

    Create new.html.erb_spec.rb

---
+++ spec/views/teams_integrations/new.html.erb_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "teams_integrations/new", type: :view do
+  # before(:each) do
+  #   assign(:teams_integration, TeamsIntegration.new(
+  #     :channel => "MyString",
+  #     :webhook_url => "MyText"
+  #   ))
+  # end
+
+  # it "renders new teams_integration form" do
+  #   render
+
+  #   assert_select "form[action=?][method=?]", teams_integrations_path, "post" do
+
+  #     assert_select "input#teams_integration_channel[name=?]", "teams_integration[channel]"
+
+  #     assert_select "textarea#teams_integration_webhook_url[name=?]", "teams_integration[webhook_url]"
+  #   end
+  # end
+end
