8674b8ba7487c2bead8bd0e2dc7e520badbce241
Matthew Verive <immewnity@users.noreply.github.com>
2021-12-30 12:13:45 -0600

    Create show.html.erb_spec.rb

---
+++ spec/views/teams_integrations/show.html.erb_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "teams_integrations/show", type: :view do
+  # before(:each) do
+  #   @teams_integration = assign(:teams_integration, TeamsIntegration.create!(
+  #     :channel => "Channel",
+  #     :webhook_url => "MyText"
+  #   ))
+  # end
+
+  # it "renders attributes in <p>" do
+  #   render
+  #   expect(rendered).to match(/Channel/)
+  #   expect(rendered).to match(/MyText/)
+  # end
+end
