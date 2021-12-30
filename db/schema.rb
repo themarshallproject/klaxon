7fad96773ba1a23ac38e84c7f2683240b09dd58c
Matthew Verive <immewnity@users.noreply.github.com>
2021-12-30 10:56:51 -0600

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_23_221320) do
+ActiveRecord::Schema.define(version: 2021_12_30_105330) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,13 @@ ActiveRecord::Schema.define(version: 2019_02_23_221320) do
 
   create_table "sqs_integrations", id: :serial, force: :cascade do |t|
     t.text "queue_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "teams_integrations", id: :serial, force: :cascade do |t|
+    t.string "channel"
+    t.text "webhook_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
