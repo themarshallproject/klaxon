70a56ba65a9829887ff46edb870c01ab48a2253c
Matthew Verive <immewnity@users.noreply.github.com>
2021-12-30 12:12:48 -0600

    Create index.html.erb_spec.rb

---
+++ spec/views/teams_integrations/index.html.erb_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "teams_integrations/index", type: :view do
+  # before(:each) do
+  #   assign(:teams_integrations, [
+  #     TeamsIntegration.create!(
+  #       :channel => "Channel",
+  #       :webhook_url => "MyText"
+  #     ),
+  #     TeamsIntegration.create!(
+  #       :channel => "Channel",
+  #       :webhook_url => "MyText"
+  #     )
+  #   ])
+  # end
+
+  # it "renders a list of teams_integrations" do
+  #   render
+  #   assert_select "tr>td", :text => "Channel".to_s, :count => 2
+  #   assert_select "tr>td", :text => "MyText".to_s, :count => 2
+  # end
+end
