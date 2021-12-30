643c657517a8bc4a0c2b51ac6c444edae23c8e89
Matthew Verive <immewnity@users.noreply.github.com>
2021-12-30 12:14:19 -0600

    Create teams_integrations_routing_spec.rb

---
+++ spec/routing/teams_integrations_routing_spec.rb
@@ -0,0 +1,39 @@
+require "rails_helper"
+
+RSpec.describe TeamsIntegrationsController, type: :routing do
+  describe "routing" do
+
+    # it "routes to #index" do
+    #   expect(:get => "/teams_integrations").to route_to("teams_integrations#index")
+    # end
+
+    # it "routes to #new" do
+    #   expect(:get => "/teams_integrations/new").to route_to("teams_integrations#new")
+    # end
+
+    # it "routes to #show" do
+    #   expect(:get => "/teams_integrations/1").to route_to("teams_integrations#show", :id => "1")
+    # end
+
+    # it "routes to #edit" do
+    #   expect(:get => "/teams_integrations/1/edit").to route_to("teams_integrations#edit", :id => "1")
+    # end
+
+    # it "routes to #create" do
+    #   expect(:post => "/teams_integrations").to route_to("teams_integrations#create")
+    # end
+
+    # it "routes to #update via PUT" do
+    #   expect(:put => "/teams_integrations/1").to route_to("teams_integrations#update", :id => "1")
+    # end
+
+    # it "routes to #update via PATCH" do
+    #   expect(:patch => "/teams_integrations/1").to route_to("teams_integrations#update", :id => "1")
+    # end
+
+    # it "routes to #destroy" do
+    #   expect(:delete => "/teams_integrations/1").to route_to("teams_integrations#destroy", :id => "1")
+    # end
+
+  end
+end
