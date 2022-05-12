6e7b6933e50644434160c148c2f9524b5c73fe10
Ryan Murphy <rdmurphy@users.noreply.github.com>
2022-05-12 10:15:23 -0700

    Only use secure cookies when force_ssl is true

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_klaxon_session', same_site: :none, secure: true
+Rails.application.config.session_store(
+  :cookie_store,
+  key: '_klaxon_session',
+  same_site: Rails.configuration.force_ssl ? :none : :lax,
+  secure: Rails.configuration.force_ssl
+)
