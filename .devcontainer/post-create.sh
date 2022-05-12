7cbdd83dd02a88356ba223d2ab21a7e77a69e125
Ryan Murphy <rdmurphy@users.noreply.github.com>
2022-05-12 14:49:17 -0700

    Add .devcontainer

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Install the version of Bundler.
+if [ -f Gemfile.lock ] && grep "BUNDLED WITH" Gemfile.lock > /dev/null; then
+    cat Gemfile.lock | tail -n 2 | grep -C2 "BUNDLED WITH" | tail -n 1 | xargs gem install bundler -v
+fi
+
+# If there's a Gemfile, then run `bundle install`
+if [ -f Gemfile ]; then
+    bundle install
+fi
+
+bin/rake db:create
+bin/rake db:migrate
