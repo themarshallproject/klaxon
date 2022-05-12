e07107f7abbf49e4edd633b075cb5ef0d7577b7c
Ryan Murphy <rdmurphy@users.noreply.github.com>
2022-05-11 18:28:40 -0700

    Make sure default config loads

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,8 @@ end
 
 module Klaxon
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.0
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
