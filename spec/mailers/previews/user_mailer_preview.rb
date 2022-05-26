7abdbe924310da544e4af355c5d8ec69f170ed83
Alexandra Andreiu <aandreiu@ucsd.edu>
2022-05-26 15:39:32 -0500

    work in progress

--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -1,4 +1,8 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
-
-end
+    def welcome_email
+        user = User.new()
+        user.id = "user_id"
+        UserMailer.with(user: user).welcome_email
+       end
+ end
