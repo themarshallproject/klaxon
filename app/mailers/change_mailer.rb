9b3d1454b3f910ecfe4bbceda25b45768e1f0188
Alexandra Andreiu <aandreiu@ucsd.edu>
2022-05-31 15:20:26 -0500

    updated subject line

--- app/mailers/change_mailer.rb
+++ app/mailers/change_mailer.rb
@@ -3,7 +3,11 @@ class ChangeMailer < ApplicationMailer
     @change = change
     @page = @change.after.page
     @user = user
-
-    mail(to: @user.email, subject: "#{@page.name} changed")
+    if ENV['KLAXON_INSTANCE_NAME'].present?
+           @instanceid = "Klaxon @ #{ENV['KLAXON_INSTANCE_NAME']}"
+        else
+           @instanceid = "Klaxon"
+     end
+    mail(to: @user.email, subject: "#{@instanceid}: #{@page.name} changed"
   end
 end
--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -1,8 +1,4 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
-    def welcome_email
-        user = User.new()
-        user.id = "user_id"
-        UserMailer.with(user: user).welcome_email
-       end
- end
+
+end
