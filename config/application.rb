43006f69604f4d76dd05276602f0477ac80bc482
Matthew Verive <immewnity@users.noreply.github.com>
2022-09-01 22:06:57 -0500

    Adds the slash at the end, plus /app/lib/

--- config/application.rb
+++ config/application.rb
@@ -35,7 +35,8 @@ module Klaxon
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.autoload_paths << "#{Rails.root}/lib"
+    config.autoload_paths << "#{Rails.root}/lib/"
+    config.autoload_paths << "#{Rails.root}/app/lib/"
 
     config.public_file_server.headers = { 'Cache-Control' => 'public, max-age=31536000' }
 
